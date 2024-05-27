aff2b42565367f77d1f32eb38ab95dbc2b4afdf4
Yousef Abu Shanab <josephyousef249@gmail.com>
2024-05-26 23:45:56 -0700

    Update template tags

--- drf_redesign/templatetags/rest_framework_redesign.py
+++ drf_redesign/templatetags/rest_framework_redesign.py
@@ -1,6 +1,5 @@
 """ Customizes template tags from DRF """
 
-
 from django.template import Library
 from django.urls import NoReverseMatch, reverse
 from django.utils.html import escape, format_html
@@ -17,16 +16,18 @@ def optional_login(request):
 
     try:
         login_url = reverse("rest_framework:login")
+
     except NoReverseMatch:
         return ""
 
     snippet = """
     <a class="dropdown-item" href="{href}?next={next}">
-        <strong class="d-flex align-items-center gap-3">
-            <i class="bi bi-box-arrow-in-right"></i>
-            Login
-        </strong>
+      <strong class="d-flex align-items-center gap-3">
+        <i class="bi bi-box-arrow-in-right"></i>
+        Login
+      </strong>
     </a>"""
+
     snippet = format_html(
         snippet,
         href=login_url,
@@ -37,11 +38,12 @@ def optional_login(request):
 
 
 @register.simple_tag
-def optional_logout(request, user):
+def optional_logout(request, user, csrf_token):
     """Include a logout snippet if REST framework's logout view is in the URLconf."""
 
     try:
         logout_url = reverse("rest_framework:logout")
+
     except NoReverseMatch:
         snippet = format_html(
             '<li class="dropdown-item">{user}</li>',
@@ -50,17 +52,23 @@ def optional_logout(request, user):
         return mark_safe(snippet)
 
     snippet = """
-    <a class="dropdown-item" href="{href}?next={next}">
+    <form method="post" action="{href}?next={next}">
+      <input type="hidden" name="csrfmiddlewaretoken" value="{csrf_token}">
+    
+      <button type="submit" class="dropdown-item">
         <strong class="d-flex align-items-center gap-3">
-            <i class="bi bi-box-arrow-right"></i>
-            Logout
+          <i class="bi bi-box-arrow-right"></i>
+          Logout
         </strong>
-    </a>"""
+      </button>
+    </form>"""
+
     snippet = format_html(
         snippet,
         user=escape(user),
         href=logout_url,
         next=escape(request.path),
+        csrf_token=csrf_token,
     )
 
     return mark_safe(snippet)
